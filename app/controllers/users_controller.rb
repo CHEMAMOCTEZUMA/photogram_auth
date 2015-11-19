a9fc0b362e976b9c7161899927165d0259c77f7b
CHEMAMOCTEZUMA <jmoctezu@chicagobooth.edu>
2015-11-19 09:37:02 -0600

    Until point 17 but 16 still needs work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @photos = @user.photos
   end
+
+  def likes
+  	@user = User.find(params[:id])
+  	@likes = @user.liked_photos
+  end
 end
